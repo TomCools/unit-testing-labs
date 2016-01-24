2cf4fdcffba9ab3fe5732f986ab8c4558cf4050b
TomCools <tom.cools@live.be>
2016-01-24 21:02:50 +0100

    Added details to ISP test and Delomboked a few classes because they weren't working in Maven.

--- unit-testing-labs-custom-matcher/src/main/java/custommatcher/domain/Adres.java
+++ unit-testing-labs-custom-matcher/src/main/java/custommatcher/domain/Adres.java
@@ -1,14 +1,69 @@
 package custommatcher.domain;
 
-import lombok.Builder;
 import lombok.Value;
 
 @Value
-@Builder
 public class Adres {
     private String street;
     private int number;
     private String bus;
     private String city;
     private String postalCode;
+
+    @java.beans.ConstructorProperties({"street", "number", "bus", "city", "postalCode"})
+    Adres(String street, int number, String bus, String city, String postalCode) {
+        this.street = street;
+        this.number = number;
+        this.bus = bus;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
+
+    public static AdresBuilder builder() {
+        return new AdresBuilder();
+    }
+
+    public static class AdresBuilder {
+        private String street;
+        private int number;
+        private String bus;
+        private String city;
+        private String postalCode;
+
+        AdresBuilder() {
+        }
+
+        public Adres.AdresBuilder street(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Adres.AdresBuilder number(int number) {
+            this.number = number;
+            return this;
+        }
+
+        public Adres.AdresBuilder bus(String bus) {
+            this.bus = bus;
+            return this;
+        }
+
+        public Adres.AdresBuilder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Adres.AdresBuilder postalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+
+        public Adres build() {
+            return new Adres(street, number, bus, city, postalCode);
+        }
+
+        public String toString() {
+            return "custommatcher.domain.Adres.AdresBuilder(street=" + this.street + ", number=" + this.number + ", bus=" + this.bus + ", city=" + this.city + ", postalCode=" + this.postalCode + ")";
+        }
+    }
 }
--- unit-testing-labs-custom-matcher/src/main/java/custommatcher/domain/Person.java
+++ unit-testing-labs-custom-matcher/src/main/java/custommatcher/domain/Person.java
@@ -1,17 +1,63 @@
 package custommatcher.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 import java.util.Date;
 
 @Value
-@Builder
 public class Person {
     private String firstName;
     private String lastName;
     private Date birthdate;
     private Adres adres;
+
+    @java.beans.ConstructorProperties({"firstName", "lastName", "birthdate", "adres"})
+    Person(String firstName, String lastName, Date birthdate, Adres adres) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.adres = adres;
+    }
+
+    public static PersonBuilder builder() {
+        return new PersonBuilder();
+    }
+
+    public static class PersonBuilder {
+        private String firstName;
+        private String lastName;
+        private Date birthdate;
+        private Adres adres;
+
+        PersonBuilder() {
+        }
+
+        public Person.PersonBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Person.PersonBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Person.PersonBuilder birthdate(Date birthdate) {
+            this.birthdate = birthdate;
+            return this;
+        }
+
+        public Person.PersonBuilder adres(Adres adres) {
+            this.adres = adres;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(firstName, lastName, birthdate, adres);
+        }
+
+        public String toString() {
+            return "custommatcher.domain.Person.PersonBuilder(firstName=" + this.firstName + ", lastName=" + this.lastName + ", birthdate=" + this.birthdate + ", adres=" + this.adres + ")";
+        }
+    }
 }
