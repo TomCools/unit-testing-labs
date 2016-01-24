245e6d8ca0404d34a2de6b1358268f8018fbee3e
TomCools <tom.cools@live.be>
2016-01-24 18:59:54 +0100

    Adjusted pom files, added Fundamentals Lab

---
+++ unit-testing-labs-fundamentals/src/test/java/gson/GsonTest.java
@@ -0,0 +1,15 @@
+package gson;
+
+import com.google.gson.Gson;
+import gson.helpclasses.User;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Schrijf voldoende testen om na te gaan of de Gson library de Objecten User en Group succesvol kan verwerken.
+ */
+public class GsonTest {
+
+}
